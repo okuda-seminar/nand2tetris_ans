d0ce2cf41bf2c8e66b8ff455e513f1463d9e85f8
Ryuju0911 <pinebook0911@gmail.com>
2023-03-07 14:53:50 +0900

    Add VM main program

---
+++ virtual_machine_1/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+#include "./src/vm_translator.hpp"
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cerr << "the arguments are invalid." << std::endl;
+    exit(-1);
+  }
+
+  std::string input = argv[1];
+  auto extension_pos = input.rfind('.');
+  if (input.substr(extension_pos) != ".vm") {
+    std::cerr << "the filename extension should be '.vm'." << std::endl;
+    exit(-1);
+  }
+  std::string output = input.substr(0, extension_pos) + ".asm";
+
+  vm::VMTranslator vm_translator(input, output);
+  vm_translator.Translate();
+
+  return 0;
+}
